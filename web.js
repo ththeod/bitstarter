4a8bc97dd198194b11533e6b9fd0b172f8890bc6
Theo Theodoridis <ththeod@gmail.com>
2013-07-12 01:31:58 +0000

    output from index.html

--- web.js
+++ web.js
@@ -2,11 +2,12 @@ var express = require('express');
 var fs = require('fs');
 
 var filename = 'index.html';
-var content = buf.toString(fs.readFileSync(filename));
+var content = new Buffer(fs.readFileSync(filename));
+var content2 = buf.toString((รร'utf8','0',content); 
 var app = express.createServer(express.logger());
 
-app.get('/', function(request, response, content) {
-  response.send(content);
+app.get('/', function(request, response, content2) {
+  response.send(content2);
 });
 
 var port = process.env.PORT || 5000;
