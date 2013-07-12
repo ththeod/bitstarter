07a8d0aca7d50587318e06052425e439c8c77fe6
Theo Theodoridis <ththeod@gmail.com>
2013-07-12 01:46:50 +0000

    hopefully working with index.html

--- web.js
+++ web.js
@@ -4,7 +4,7 @@ var fs = require('fs');
 var str = fs.readFileSync("index.html",[encoding="utf8"]);
 var app = express.createServer(express.logger());
 
-app.get('/', function(request, response, content2) {
+app.get('/', function(request, response) {
   response.send(str);
 });
 
