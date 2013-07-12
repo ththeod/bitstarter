7b4b0bb993aad4f450272167cba6819d4cbe3d57
Theo Theodoridis <ththeod@gmail.com>
2013-07-12 01:45:00 +0000

    hopefully working with index.html

--- web.js
+++ web.js
@@ -1,13 +1,11 @@
 var express = require('express');
 var fs = require('fs');
 
-var filename = 'index.html';
-var content = new Buffer(fs.readFileSync(filename));
-var content2 = buf.toString((รร'utf8','0',content); 
+var str = fs.readFileSync("index.html",[encoding="utf8"]);
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response, content2) {
-  response.send(content2);
+  response.send(str);
 });
 
 var port = process.env.PORT || 5000;
